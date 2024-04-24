1f4ca950384e765b73b803df21749b8d765720b5
tina1461 <tina8243@gmail.com>
2024-04-24 18:01:05 +0800

    feat: Add item & Delete item

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Button, Input, Label } from "@/components/ui";
+import { XCircleIcon } from "@heroicons/react/24/solid";
 
 function App() {
+  const [itemName, setItemName] = useState("");
+  const [shoppingList, setShoppingList] = useState<string[]>([]);
+
+  const addingItem = () => {
+    if (itemName.length === 0) return;
+    setShoppingList(prevState => [...prevState, itemName.trim()]);
+    setItemName("");
+  };
+
+  const removeItem = (index: number) => {
+    const newArr = shoppingList.filter((_, itemIndex) => itemIndex !== index);
+    setShoppingList(newArr);
+  };
+
   return (
     <>
-      <Button> Click me</Button>
+      <div className="p-5 flex items-center space-x-2">
+        <Input
+          placeholder="Item Name"
+          value={itemName}
+          onChange={e => setItemName(e.target.value)}
+        />
+        <Button
+          className="text-center"
+          onClick={addingItem}
+          disabled={itemName.length === 0}
+        >
+          Add item
+        </Button>
+      </div>
+      <div className="p-5">
+        {shoppingList.map((item, itemIndex) => (
+          <div className="flex items-center space-x-2">
+            <Label htmlFor="">{item}</Label>
+            <Button
+              className="text-center"
+              variant="outline"
+              size="icon"
+              onClick={() => removeItem(itemIndex)}
+            >
+              <XCircleIcon className="h-6 w-6" />
+            </Button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
